6fb39e98d1d3f8b513fcbe483854e0325e35f62c
Gabriel Sancho <gabriel.cavalcanti@sicoob.com.br>
2022-02-19 10:56:34 -0300

    First commit

---
+++ src/main/kotlin/dev/panelinha/games/lobby/LobbyApplication.kt
@@ -0,0 +1,11 @@
+package dev.panelinha.games.lobby
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LobbyApplication
+
+fun main(args: Array<String>) {
+	runApplication<LobbyApplication>(*args)
+}
